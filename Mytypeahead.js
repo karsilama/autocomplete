4e436a3a1612c5749c36cde853934d24d532b1bc
karsilama <jesus.ac@gmail.com>
2016-09-03 19:58:49 -0300

    mytypeahead

---
+++ Mytypeahead.js
@@ -0,0 +1,45 @@
+
+			function Mytypeahead( settings ) {
+
+				var self = this,
+
+						request = false,
+
+						search = function(){
+													$.ajax(
+															{
+																url:settings.url,
+																type:"POST",
+																dataType:"JSON",
+																data : {
+																	table:settings.table,
+																	field:settings.field,
+																	string: $.trim(self.selector.val())
+																},
+																success : function(data){
+																	if ( data && data.source.length > 0 ){
+																		self.selector.typeahead( {
+																				source: data.source,
+																				displayField: 'name',
+																				updater : function(item){
+																					self.selector.attr("data-id", item.id )
+																					self.selector.attr("placeholder",item.name )
+																				}
+																			} )
+																	} else {
+																		self.selector.attr("placeholder",self.placeholder )
+																	}
+																}
+															}
+														)
+										} ;
+
+				self.selector = $(settings.selector).bind("click", function(){
+					$(this).val("")
+				}) ;
+
+				return function(){
+					self.placeholder = self.selector.attr("placeholder")
+					search()
+				}()
+			}
